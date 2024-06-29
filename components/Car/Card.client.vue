36588db6aba4e1b9eca303cb2b64f0c1144f1782
akosczabai <czabai.akos@gmail.com>
2024-06-29 12:29:00 +0200

    using local storage

---
+++ components/Car/Card.client.vue
@@ -0,0 +1,33 @@
+<script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+const props = defineProps({
+  car: Object,
+  favored: Boolean,
+});
+
+const emit = defineEmits(["favor"]);
+</script>
+
+<template>
+  <div
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <img
+      @click="emit('favor', car.id)"
+      class="absolute w-7 right-5 top-2 z-20"
+      :src="favored ? heartFilled : heartOutline"
+      alt=""
+    />
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          <p class="tex-threx-700">{{ car.description }}</p>
+        </div>
+        <h1 class="mt-auto text-xl">{{ car.price }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
